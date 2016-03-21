1bcf49662401a930c3f31350eb21018209009ae8
rowanv <rrvspam@gmail.com>
2016-03-21 09:11:37 +0000

    Changed unit tests so all Selenium calls are mocked out, added non-numeric metric error handling and tests.

--- client_wrapper/client_wrapper.py
+++ client_wrapper/client_wrapper.py
@@ -12,14 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import foo
-import unittest
 from datetime import datetime
 import pytz
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import WebDriverException, TimeoutException
 
@@ -31,12 +27,10 @@ class TestError(object):
         timestamp: Datetime of when the error was observed
         message: String message describing the error.
     '''
+
     def __init__(self, timestamp, message):
         self.timestamp = timestamp
         self.message = message
-
-
-
 
 
 class NdtResult(object):
@@ -59,13 +53,22 @@ class NdtResult(object):
             encountered during the tests (or an empty list if all
             tests were successful).
     '''
-    def __init__(self, start_time, end_time, c2s_start_time, s2c_start_time,
-        errors, latency=None, upload_speed=None, download_speed=None):
+
+    def __init__(self,
+                 start_time,
+                 end_time,
+                 c2s_start_time,
+                 s2c_start_time,
+                 errors,
+                 latency=None,
+                 upload_speed=None,
+                 download_speed=None):
         self.start_time = start_time
         self.end_time = end_time
         self.c2s_start_time = c2s_start_time
         self.s2c_start_time = s2c_start_time
         self.errors = errors
+
         if latency:
             self.latency = latency
         if upload_speed:
@@ -73,9 +76,25 @@ class NdtResult(object):
         if download_speed:
             self.download_speed = download_speed
 
+    def __str__(self):
+        return 'NDT Results:\n Start Time: %s,\n End Time: %s'\
+        ',\n Latency: %s, \nErrors: %s' % (
+            self.start_time, self.end_time, self.latency, self.errors)
 
 
 class NdtHtml5SeleniumDriver(object):
+    '''
+    Attributes:
+        result_values: A dictionary containing the results of
+            an NDT test.
+    '''
+
+    def __init__(self):
+        self.result_values = {'start_time': None,
+                              'end_time': None,
+                              'c2s_start_time': None,
+                              's2c_start_time': None,
+                              'errors': []}
 
     def set_test_browser(self, browser):
         '''
@@ -89,6 +108,33 @@ class NdtHtml5SeleniumDriver(object):
         '''
         if browser == 'firefox':
             return webdriver.Firefox()
+
+    def check_result_metrics(self):
+        '''
+        Checks that the values for upload speed, download
+        speed, and latency within the result_values dict are valid.
+        '''
+        self.test_indiv_metric_is_valid('latency')
+        self.test_indiv_metric_is_valid('download_speed')
+        self.test_indiv_metric_is_valid('upload_speed')
+
+    def test_indiv_metric_is_valid(self, metric):
+        '''
+        For a given metric in result_values, checks that it is
+        a valid numeric value. If not, an error is added to
+        result_values' errors list.
+
+        Args:
+            metric: One of 'latency', 'download_speed', or
+                'upload_speed'.
+        '''
+        try:
+            float(self.result_values[metric])
+        except ValueError:
+            message = 'illegal value shown for ' + \
+                metric + ': ' + str(self.result_values[metric])
+            self.result_values['errors'].append(TestError(
+                datetime.now(pytz.utc), message))
 
     def perform_test(self, url, browser, timeout_time=20):
         '''
@@ -107,71 +153,67 @@ class NdtHtml5SeleniumDriver(object):
             A populated NdtResult object
         '''
         self.driver = self.set_test_browser(browser)
-        result_values = {'start_time': None,
-                'end_time': None,
-                'c2s_start_time': None,
-                's2c_start_time': None,
-                'errors': []
-                }
-
 
         try:
             self.driver.get(url)
-
-        except WebDriverException, e:
+        except WebDriverException as e:
             if e.msg == u'Target URL invalid_url is not well-formed.':
-                result_values['errors'].append(TestError(datetime.now(pytz.utc), e.msg))
+                self.result_values['errors'].append(TestError(
+                    datetime.now(pytz.utc), e.msg))
                 self.driver.close()
-                return NdtResult(**result_values)
-
+                return NdtResult(**self.result_values)
         self.driver.find_element_by_id('websocketButton').click()
 
         start_button = self.driver.find_elements_by_xpath(
             "//*[contains(text(), 'Start Test')]")[0]
         start_button.click()
-        result_values['start_time'] = datetime.now(pytz.utc)
+        self.result_values['start_time'] = datetime.now(pytz.utc)
 
         try:
             # wait until 'Now Testing your upload speed' is displayed
             upload_speed_text = self.driver.find_elements_by_xpath(
                 "//*[contains(text(), 'your upload speed')]")[0]
-            element = WebDriverWait(self.driver, timeout=timeout_time).until(
-            EC.visibility_of(upload_speed_text))
-            result_values['c2s_start_time'] = datetime.now(pytz.utc)
+            WebDriverWait(
+                self.driver,
+                timeout=timeout_time).until(EC.visibility_of(upload_speed_text))
+            self.result_values['c2s_start_time'] = datetime.now(pytz.utc)
 
             # wait until 'Now Testing your download speed' is displayed
             download_speed_text = self.driver.find_elements_by_xpath(
                 "//*[contains(text(), 'your download speed')]")[0]
-            element = WebDriverWait(self.driver, timeout=timeout_time).until(
-            EC.visibility_of(download_speed_text))
-            result_values['s2c_start_time'] = datetime.now(pytz.utc)
+            WebDriverWait(self.driver,
+                          timeout=timeout_time).until(EC.visibility_of(
+                              download_speed_text))
+            self.result_values['s2c_start_time'] = datetime.now(pytz.utc)
 
             # wait until the results page appears
             results_text = self.driver.find_element_by_id('results')
-            WebDriverWait(self.driver, timeout=timeout_time).until(
-                EC.visibility_of(results_text))
-            result_values['end_time'] = datetime.now(pytz.utc)
+            WebDriverWait(
+                self.driver,
+                timeout=timeout_time).until(EC.visibility_of(results_text))
+            self.result_values['end_time'] = datetime.now(pytz.utc)
 
-            result_values['upload_speed'] = self.driver.find_element_by_id(
+            # Find metric values
+            self.result_values['upload_speed'] = self.driver.find_element_by_id(
                 'upload-speed').text
-            result_values['download_speed'] = self.driver.find_element_by_id(
-                'download-speed').text
-
-            result_values['latency'] = self.driver.find_element_by_id(
+            self.result_values[
+                'download_speed'] = self.driver.find_element_by_id(
+                    'download-speed').text
+            self.result_values['latency'] = self.driver.find_element_by_id(
                 'latency').text
-        except TimeoutException, e:
-            result_values['errors'].append(TestError(datetime.now(pytz.utc),
-                'Test did not complete within timeout period.'))
+
+        except TimeoutException as e:
+            message = 'Test did not complete within timeout period.'
+            self.result_values['errors'].append(TestError(
+                datetime.now(pytz.utc), message))
             self.driver.close()
-            return NdtResult(**result_values)
+            return NdtResult(**self.result_values)
 
-        print(result_values)
-
+        self.check_result_metrics()
 
         self.driver.close()
 
-        return NdtResult(**result_values)
-
+        return NdtResult(**self.result_values)
 
 
 def main():
@@ -180,7 +222,7 @@ def main():
         url='http://ndt.iupui.mlab4.nuq1t.measurement-lab.org:7123/',
         browser='firefox')
     print(test_results)
-    print(test_results.latency)
+
 
 if __name__ == '__main__':
     main()
--- tests/test_client_wrapper.py
+++ tests/test_client_wrapper.py
@@ -12,53 +12,113 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 from __future__ import absolute_import
-import os
-import sys
 import unittest
 import mock
 from client_wrapper import client_wrapper
-import selenium
-
-sys.path.insert(1, os.path.abspath(os.path.join(
-    os.path.dirname(__file__), '../client_wrapper')))
-import foo
 
 
 class ClientWrapperTest(unittest.TestCase):
 
-    def test_run_tests_executes_all_tests_successfully(self):
-        """Dummy test to exercise CI setup, not permanent."""
-        self.assertTrue(foo.bar())
+    def test_results_page_displays_non_numeric_metrics(self):
+        '''
+        When latency, upload_speed, and download_speed are
+        non-numeric values, corresponding error objects are
+        added to the errors list that indicates that the each
+        of these values is invalid.
+        '''
+        with mock.patch(
+                'client_wrapper.client_wrapper.NdtHtml5SeleniumDriver.set_test_browser',
+                autospec=True) as mock_set_test_browser:
+            # mock driver
+            mock_set_test_browser.return_value = mock.MagicMock()
+
+            # Set values read from page for latency, upload speed,
+            # download speed
+            mock_set_test_browser.return_value.find_element_by_id(
+                'latency').text = 'Non numeric value'
+            mock_set_test_browser.return_value.find_element_by_id(
+                'upload_speed').text = 'Non numeric value'
+            mock_set_test_browser.return_value.find_element_by_id(
+                'download_speed').text = 'Non numeric value'
+
+            # Mock visibility function so shows all elements are
+            # always visible
+            with mock.patch(
+                    'selenium.webdriver.support.expected_conditions.visibility_of') as mock_visibility:
+                mock_visibility = mock.Mock()
+                mock_visibility.return_value = True
+
+                selenium_driver = client_wrapper.NdtHtml5SeleniumDriver()
+                test_results = selenium_driver.perform_test(
+                    url=
+                    'http://ndt.iupui.mlab4.nuq1t.measurement-lab.org:7123/',
+                    browser='firefox',
+                    timeout_time=1000)
+
+                # And the appropriate error objects are contained in
+                # the list
+                self.assertEqual(
+                    test_results.errors[0].message,
+                    'illegal value shown for latency: Non numeric value')
+                self.assertEqual(
+                    test_results.errors[1].message,
+                    'illegal value shown for download_speed: Non numeric value')
+                self.assertEqual(
+                    test_results.errors[2].message,
+                    'illegal value shown for upload_speed: Non numeric value')
+
+    def test_results_page_displays_numeric_latency(self):
+        '''
+        When latency is a numeric value, an error object is
+        not added to the errors list.
+        '''
+        with mock.patch(
+                'client_wrapper.client_wrapper.NdtHtml5SeleniumDriver.set_test_browser',
+                autospec=True) as mock_set_test_browser:
+            # mock driver
+            mock_set_test_browser.return_value = mock.MagicMock()
+            mock_set_test_browser.return_value.find_element_by_id(
+                'latency').text = '72'
+            with mock.patch(
+                    'selenium.webdriver.support.expected_conditions.visibility_of') as mock_visibility:
+                mock_visibility = mock.Mock()
+                mock_visibility.return_value = True
+
+                selenium_driver = client_wrapper.NdtHtml5SeleniumDriver()
+                test_results = selenium_driver.perform_test(
+                    url=
+                    'http://ndt.iupui.mlab4.nuq1t.measurement-lab.org:7123/',
+                    browser='firefox',
+                    timeout_time=1000)
+                # And an error object is not contained in the list
+                self.assertEqual(len(test_results.errors), 0)
 
     def test_invalid_URL_throws_error(self):
         selenium_driver = client_wrapper.NdtHtml5SeleniumDriver()
-        test_results = selenium_driver.perform_test(
-            url='invalid_url',
-            browser='firefox')
+        test_results = selenium_driver.perform_test(url='invalid_url',
+                                                    browser='firefox')
 
         # We have one error
         self.assertEqual(len(test_results.errors), 1)
 
-        # And that error is about the test results URL not being well-formed
+        # And that error is about the test results URL not being
+        # well-formed
         self.assertEqual(test_results.errors[0].message,
-            u'Target URL invalid_url is not well-formed.')
+                         u'Target URL invalid_url is not well-formed.')
 
     def test_timeout_throws_error(self):
         selenium_driver = client_wrapper.NdtHtml5SeleniumDriver()
         test_results = selenium_driver.perform_test(
             url='http://ndt.iupui.mlab4.nuq1t.measurement-lab.org:7123/',
-            browser='firefox', timeout_time=1)
+            browser='firefox',
+            timeout_time=1)
 
         # We have one error
         self.assertEqual(len(test_results.errors), 1)
 
         # And that is a timout error
         self.assertEqual(test_results.errors[0].message,
-            'Test did not complete within timeout period.')
-
-
-
-
+                         'Test did not complete within timeout period.')
 
 
 if __name__ == '__main__':
